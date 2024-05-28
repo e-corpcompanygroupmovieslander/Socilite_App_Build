7d6ee1f742cafd7d176b0b678fc5ee155d7d70f5
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-05-28 14:19:10 +0300

    test one

--- app/src/main/res/raw/environment.js
+++
@@ -1,4 +0,0 @@
-ENVIRONMENT('Test');BODY();ZOOM();
-STORE('local','FrameWork','Advance-Native');STORE('local','FrameWorkVersion','0.0.1');
-STORE('local','Device','Android');CLICKED('body',()=>{HIDENAVIGATIONBAR();NETWORKED();});
-NETWORKED();HIDENAVIGATIONBAR();
--- app/src/main/res/raw/main.js
+++
@@ -1,1 +0,0 @@
-const BODY=()=>{ DECLARATION('body',(ELEMENT)=>{ STYLED(ELEMENT,'position','fixed'); STYLED(ELEMENT,'width','100%'); STYLED(ELEMENT,'height','auto'); STYLED(ELEMENT,'bottom','0'); STYLED(ELEMENT,'margin','0'); STYLED(ELEMENT,'padding','0'); STYLED(ELEMENT,'display','block'); STYLED(ELEMENT,'overflow','hidden'); STYLED(ELEMENT,'background','transparent'); }) };
