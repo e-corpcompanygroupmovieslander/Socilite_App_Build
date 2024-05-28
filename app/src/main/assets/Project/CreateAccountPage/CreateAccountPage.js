a7ae855af4ca8da8975c4e4be018a859d305bd71
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-05-28 21:54:09 +0300

    CREATE ACCOUNT PAGE

--- app/src/main/assets/Project/CreateAccountPage/CreateAccountPage.js
+++ app/src/main/assets/Project/CreateAccountPage/CreateAccountPage.js
@@ -4,7 +4,7 @@ export const CREATEACCOUNTPAGE=()=>{
 
         <h2>Socilite</h2>
 
-        <input type='email' class='Email' placeholder='Enter User Name' />
+        <input type='text' class='Name' placeholder='Enter User Name' />
 
         <input type='email' class='Email' placeholder='Enter User Email' />
 
@@ -15,5 +15,71 @@ export const CREATEACCOUNTPAGE=()=>{
         <button class='blue'>LogIn</button>
 
     `);
+
+
+    const Name=document.querySelector('.Name');
+    const Email=document.querySelector('.Email');
+    const Password=document.querySelector('.Password');
+    const Button=document.querySelector('.forestgreen');
+
+    CLICKED('.forestgreen',()=>{
+
+        CONDITION(Name.value,
+
+            ()=>CONDITION(Email.value,
+
+                ()=>CONDITION(Password.value,
+
+                    ()=>alert(''),
+        
+                    ()=>CHECK(Password,(result)=>{
+        
+                        VIBRATION(500);
+                        
+                        STYLED(Password,'border-bottom','1px solid red');
+        
+                        setTimeout(() => {
+        
+                            STYLED(Password,'border-bottom','1px solid #cdcdcd20');
+                            
+                        }, 2000);
+        
+                    })
+        
+                ),
+    
+                ()=>CHECK(Name,(result)=>{
+    
+                    VIBRATION(500);
+                    
+                    STYLED(Email,'border-bottom','1px solid red');
+    
+                    setTimeout(() => {
+    
+                        STYLED(Email,'border-bottom','1px solid #cdcdcd20');
+                        
+                    }, 2000);
+    
+                })
+    
+            ),
+
+            ()=>CHECK(Name,(result)=>{
+
+                VIBRATION(500);
+                
+                STYLED(Name,'border-bottom','1px solid red');
+
+                setTimeout(() => {
+
+                    STYLED(Name,'border-bottom','1px solid #cdcdcd20');
+                    
+                }, 2000);
+
+            })
+
+        );
+
+    })
 
 }
