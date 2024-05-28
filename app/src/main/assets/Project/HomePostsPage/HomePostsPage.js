ca08464514a575b701d126f3f205cf11f2d2da60
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-05-28 16:11:35 +0300

    HOME PAGE POSTS

--- app/src/main/assets/Project/HomePostsPage/HomePostsPage.js
+++ app/src/main/assets/Project/HomePostsPage/HomePostsPage.js
@@ -8,7 +8,7 @@ export const HOMEPOSTSPAGE=()=>{
 
             REDUX(data,(Element)=>{
     
-                REDUX(Element.Posts,(element)=>{
+                REDUX(Element.Posts.reverse(),(element)=>{
     
                     const DIV=document.createElement('div');
     
@@ -37,6 +37,57 @@ export const HOMEPOSTSPAGE=()=>{
                     USERNAME.innerHTML=element.PostersName;
 
                     ADD(DIV,USERNAME);
+
+                    const USERLOCATION=document.createElement('h1');
+    
+                    USERLOCATION.classList.add('UserLocation');
+
+                    USERLOCATION.innerHTML=element.PostsLocation||'';
+
+                    ADD(DIV,USERLOCATION);
+
+                    TIMER(element.PostTime,(time)=>{
+  
+                        const USERTIME=document.createElement('h1');
+        
+                        USERTIME.classList.add('UserTime');
+
+                        USERTIME.innerHTML=time;
+
+                        ADD(DIV,USERTIME);
+                    })
+
+                    const MINIDIV=document.createElement('div');
+    
+                    MINIDIV.classList.add('OptionsDiv');
+
+                    const LIKEICON=document.createElement('img');
+    
+                    LIKEICON.src=WHITEICONS+'heart.png';
+
+                    ADD(MINIDIV,LIKEICON);
+
+                    const COMMENTICON=document.createElement('img');
+    
+                    COMMENTICON.src=WHITEICONS+'comment.png';
+
+                    ADD(MINIDIV,COMMENTICON);
+
+                    const SHAREICON=document.createElement('img');
+    
+                    SHAREICON.src=WHITEICONS+'save.png';
+
+                    ADD(MINIDIV,SHAREICON);
+
+                    ADD(DIV,MINIDIV);
+
+                    const DESCRIPTION=document.createElement('p');
+        
+                    DESCRIPTION.classList.add('UserPosterDescription');
+
+                    DESCRIPTION.innerHTML=element.Description||'No Story';
+
+                    ADD(DIV,DESCRIPTION);
 
                     ADD(ELEMENT,DIV);
     
