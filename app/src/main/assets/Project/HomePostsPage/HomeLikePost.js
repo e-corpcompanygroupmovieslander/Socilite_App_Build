9d4dfd8338a87fe1b2fa63dd3f7cf1d114e5dae2
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-05-31 06:05:36 +0300

    TEST

--- app/src/main/assets/Project/HomePostsPage/HomeLikePost.js
+++ app/src/main/assets/Project/HomePostsPage/HomeLikePost.js
@@ -1,22 +1,62 @@
-export const HOMELIKEPOSTS=(MINIDIV,element)=>{
+import { UPDATEPOSTSAPI } from "../Apis/Apis.js";
+import { AUTODOWNLOADPOSTS } from "../AutoLoginPage/AutoDownloadPosts.js";
 
-    const LIKEICON=document.createElement('img');
+export const HOMELIKEPOSTS = (MINIDIV, element) => {
+    const LIKEICON = document.createElement('img');
 
-    CONDITION(element.PeopleLiked.includes(localStorage.getItem('User')),
+    // Initialize PeopleLiked as an array if it isn't already
+    if (!Array.isArray(element.PeopleLiked)) {
+        element.PeopleLiked = [];
+    }
 
-        ()=>LIKEICON.src=WHITEICONS+'heart.png',
+    // Retrieve liked users from session storage or initialize as an empty array
+    let likedUsers = JSON.parse(sessionStorage.getItem('LikedUsers')) || [];
 
-        ()=>LIKEICON.src=WHITEICONS+'unheart.png'
+    const updateLikeIcon = () => {
+        if (element.PeopleLiked.includes(localStorage.getItem('User'))) {
+            LIKEICON.src = WHITEICONS + 'heart.png';
+        } else {
+            LIKEICON.src = WHITEICONS + 'unheart.png';
+        }
+    };
 
-    );
+    updateLikeIcon();
 
-    
-    ADD(MINIDIV,LIKEICON);
+    ADD(MINIDIV, LIKEICON);
 
-    EVENT(LIKEICON,'click',()=>{
+    EVENT(LIKEICON, 'click', () => {
+        const user = localStorage.getItem('User');
 
-        alert('')
+        if (element.PeopleLiked.includes(user)) {
+            // Remove user from PeopleLiked array
+            element.PeopleLiked = element.PeopleLiked.filter(u => u !== user);
+            // Remove user from likedUsers array
+            likedUsers = likedUsers.filter(likedUser => likedUser !== user);
+        } else {
+            // Add user to PeopleLiked array
+            element.PeopleLiked.push(user);
+            // Add user to likedUsers array
+            likedUsers.push(user);
+        }
 
+        // Save the liked users array to session storage
+        sessionStorage.setItem('LikedUsers', JSON.stringify(likedUsers));
+
+        // Save the updated post data to session storage (if needed)
+        sessionStorage.setItem('CurrentPosts', JSON.stringify(element));
+
+        DEJSON('', 'CurrentPosts', (data) => {
+            const USERS = {
+                "UserID": data.UserID,
+                "PeopleLiked": data.PeopleLiked
+            };
+
+            POSTPACKAGE(UPDATEPOSTSAPI, 'no-cors', USERS, (info) => {
+                AUTODOWNLOADPOSTS();
+            });
+        });
+
+        // Update the icon
+        updateLikeIcon();
     });
-   
-}
+};
