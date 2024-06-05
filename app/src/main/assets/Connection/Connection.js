c6929a53915f4c6c0942880e9aa3dfdecb5c5b28
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-06-05 20:31:37 +0300

    LAUNCH ONE

--- app/src/main/assets/Connection/Connection.js
+++ app/src/main/assets/Connection/Connection.js
@@ -1,11 +1,7 @@
-import { AUTODOWNLOADPOSTS } from "../Project/AutoDownloadPage/AutoDownloadPosts.js";
-import { AUTOLOGINPAGE } from "../Project/AutoDownloadPage/AutoLoginPage.js";
 import { HOMEPAGE } from "../Project/HomePage/HomePage.js"
 
 export const CONNECTION=()=>{
 
-    AUTODOWNLOADPOSTS();
-
-    AUTOLOGINPAGE();
+    HOMEPAGE();
 
 }
